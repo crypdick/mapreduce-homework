c9f4253a755861be2b26753d679c7015c4f660d4
Richard Decal <richard.decal@ncf.edu>
2018-02-10 10:45:32 -0500

    implemented alphebetized counter

--- total_alphabetized.py
+++ total_alphabetized.py
@@ -1,12 +1,12 @@
 from mrjob.job import MRJob
 
-class WordCounter(MRJob):
+class AlphabeticCounter(MRJob):
     def mapper(self, _, line):
         for word in line.split():
-            yield (_, 1)
+            yield (word[0].lower(), 1)
 
     def reducer(self, key, values):
         yield (key, sum(values))
 
 if __name__ == '__main__':
-    WordCounter.run()
+    AlphabeticCounter.run()
