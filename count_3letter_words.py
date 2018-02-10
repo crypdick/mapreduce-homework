33ef9572788ca68ede86f4216d50abbcb44dfe7f
Richard Decal <richard.decal@ncf.edu>
2018-02-10 10:38:35 -0500

    implemented 3 word counter

--- count_3letter_words.py
+++ count_3letter_words.py
@@ -1,14 +1,14 @@
 from mrjob.job import MRJob
 
-class TheCounter(MRJob):
+class ThreeLetterWordCounter(MRJob):
     def mapper(self, _, line):
         for word in line.split():
             yield (word, 1)
 
     def reducer(self, key, values):
-        if key != 'the':
+        if len(key) != 3:
             return None
         yield (key, sum(values))
 
 if __name__ == '__main__':
-    TheCounter.run()
+    ThreeLetterWordCounter.run()
