9becbb0276efcdad8feb1690dcb72726cdfa3ae1
Richard Decal <richard.decal@ncf.edu>
2018-02-10 10:55:37 -0500

    implemented, I think

--- avg_vowelcount_per_wordlen.py
+++ avg_vowelcount_per_wordlen.py
@@ -1,14 +1,20 @@
 from mrjob.job import MRJob
 
-class ThreeLetterWordCounter(MRJob):
+class AvgNVowelsPerWordlen(MRJob):
     def mapper(self, _, line):
         for word in line.split():
-            yield (word, 1)
+            yield (len(word), word)
 
-    def reducer(self, key, values):
-        if len(key) != 3:
-            return None
-        yield (key, sum(values))
+    def reducer(self, key, words):
+        num_words = 0
+        num_vowels = 0
+        for word in words:
+            num_words += 1
+            for char in list(word):
+                if char.lower() in 'aeiou':
+                    num_vowels += 1
+
+        yield (key, num_vowels/num_words)
 
 if __name__ == '__main__':
-    ThreeLetterWordCounter.run()
+    AvgNVowelsPerWordlen.run()
